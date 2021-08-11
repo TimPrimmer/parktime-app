64d482cc789ae225445996c2c8011341fb8e4e60
Erin Holmes <e.holmes09@yahoo.com>
2021-08-11 13:38:10 -0500

    set up result-routes to feed temp data to parks.handlebars

--- controllers/result-routes.js
+++ controllers/result-routes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
+const { parks } = require("../data/parks.json");
 
 router.get('/', (req, res) => {
-  res.render("parks");
+  res.render("parks", {parks});
 });
 
 module.exports = router;
