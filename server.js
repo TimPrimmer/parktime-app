eaec3b38d1f0973445be5c69c909185808df751e
Erin Holmes <e.holmes09@yahoo.com>
2021-08-12 21:55:40 -0500

    update server changes from merge with develop

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers/');
-<<<<<<< HEAD
 const exphbs = require('express-handlebars');
-=======
 const sequelize = require('./config/connection');
 const cors = require('cors');
->>>>>>> develop
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-<<<<<<< HEAD
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-=======
 app.use(cors());
->>>>>>> develop
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
