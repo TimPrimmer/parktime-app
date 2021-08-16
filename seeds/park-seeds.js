534d923edb2d2cea64007ccc2c7e97bacbf17ae9
Erin Holmes <e.holmes09@yahoo.com>
2021-08-16 17:31:59 -0500

    update seeds file to add categories to park's activities

--- seeds/park-seeds.js
+++ seeds/park-seeds.js
@@ -49,7 +49,10 @@ const generateSqlArray = (parkData) => {
 
 const seedParks = async () => {
   const parkData = await getParkData();
-
+  // update parks.json file with categories 
+  for (let park of parkData.parks) {
+    updateActivitiesArray(park.activities);
+  }
   writeToFile("./data/parks.json", JSON.stringify(parkData))
 
   const parkSql = generateSqlArray(parkData);
@@ -58,5 +61,61 @@ const seedParks = async () => {
   console.log('Parks seeded!');
 }
 
-module.exports = seedParks;
+var rangerProgram = ["Junior Ranger Program"];
+var wildlifeWatching = ["Wildlife Watching", "Birdwatching", "Scenic Driving"];
+var artsAndScience = ["Stargazing", "Hands-On", "Park Film", "Arts and Crafts", "Live Music", "Arts and Culture", "Theater", "Astronomy", "Citizen Science", "Planetarium"];
+var historyAndCulture = ["Museum Exhibits", "Living History", "Cultural Demonstrations", "Historic Weapons Demonstration", "Craft Demonstrations", "First Person Interpretation", "Reenactments"];
+var waterActivities = ["Fishing", "Paddling", "Boating", "Kayaking", "Canoeing", "Boat Tour", "Freshwater Fishing", "Swimming", "Stand Up Paddleboarding", "Fly Fishing", "Saltwater Fishing", "Motorized Boating", "Saltwater Swimming", "Freshwater Swimming", "SCUBA Diving", "Tubing", 'Sailing', "Whitewater Rafting", "Snorkeling", "Surfing", "River Tubing", "Water Skiing", "Jet Skiing", "Pool Swimming"];
+var shopping = ["Shopping", "Bookstore and Park Store", "Gift Shop and Souvenirs"];
+var hikingAndClimbing = ["Hiking", "Front-Country Hiking", "Backcountry Hiking", "Off-Trail Permitted Hiking", "Climbing", "Rock Climbing", "Mountain Climbing"];
+var tours = ["Guided Tours", "Self-Guided Tours - Walking", "Self-Guided Tours - Auto", "Bus/Shuttle Guided Tour"];
+var foodAndDining = ["Picnicking", "Food", "Dining"];
+var biking = ["Road Biking", "Mountain Biking", "Biking"];
+var camping = ["Horse Camping (see also Horse/Stock Use)", "Horse Camping (see also camping)", "Camping", "Backcountry Camping", "Car or Front Country Camping", "Canoe or Kayak Camping", "Group Camping", "RV Camping"];
+var winterActivities = ["Skiing", "Snowshoeing", "Cross-Country Skiing", "Snow Play", "Snowmobiling", "Downhill Skiing", "Snow Tubing", "Ice Climbing", "Dog Sledding", "Ice Skating"];
 
+var updateActivitiesArray = function (activities) {
+  for (var activity of activities) {
+
+    if (rangerProgram.includes(activity.name)) {
+      activity["category"] = "Junior Ranger Program";
+    }
+    if (wildlifeWatching.includes(activity.name)) {
+      activity["category"] = "Wildlife Watching";
+    }
+    if (artsAndScience.includes(activity.name)) {
+      activity["category"] = "Arts and Science";
+    }
+    if (historyAndCulture.includes(activity.name)) {
+      activity["category"] = "History and Culture";
+    }
+    if (waterActivities.includes(activity.name)) {
+      activity["category"] = "Water Activities";
+    }
+    if (shopping.includes(activity.name)) {
+      activity["category"] = "Shopping";
+    }
+    if (hikingAndClimbing.includes(activity.name)) {
+      activity["category"] = "Hiking and Climbing";
+    }
+    if (tours.includes(activity.name)) {
+      activity["category"] = "Tours";
+    }
+    if (foodAndDining.includes(activity.name)) {
+      activity["category"] = "Food and Dining";
+    }
+    if (biking.includes(activity.name)) {
+      activity["category"] = "Biking";
+    }
+    if (camping.includes(activity.name)) {
+      activity["category"] = "Camping";
+    }
+    if (winterActivities.includes(activity.name)) {
+      activity["category"] = "Winter Activities";
+    }
+  }
+}
+
+
+
+module.exports = seedParks;
