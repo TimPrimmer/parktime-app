d9305822853583c43e405e6bd4082bb9d95ce5b3
Tim Primmer <timprimmer@gmail.com>
2021-08-16 12:25:45 -0500

    commented out user routes for now (they dont exist)

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
+//const userRoutes = require('./user-routes.js');
 const parkRoutes = require('./park-routes.js');
 
-router.use('/users', userRoutes);
+//router.use('/users', userRoutes);
 router.use('/parks', parkRoutes);
 
 module.exports = router;
