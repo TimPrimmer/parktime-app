f2a08b3f9349e60a85252a149d8d048fa45fbc6f
Derek Beckford <dbeck1207@icloud.com>
2021-08-16 20:35:54 -0500

    comment routes logic

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,48 @@
+const router = require("express").Router();
+const sequelize = require("../../config/connection");
+const { Comment } = require("../../models");
+
+router.get("/", (req, res) => {
+  Comment.findAll({})
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      user_id: req.body.user_id, //add session
+      park_id: req.body.park_id,
+    })
+      .then((dbCommentData) => res.json(dbCommentData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCommentData) => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: "No comment found with this id" });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const userRoutes = require('./user-routes.js');
-const parkRoutes = require('./park-routes.js');
+const userRoutes = require("./user-routes.js");
+const parkRoutes = require("./park-routes.js");
+const commentRoutes = require("./comment-routes");
 
-
-router.use('/users', userRoutes);
-router.use('/parks', parkRoutes);
-
+router.use("/users", userRoutes);
+router.use("/parks", parkRoutes);
+router.use("/comments", commentRoutes);
 
 module.exports = router;
