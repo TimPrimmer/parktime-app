f6b5fb958aa7fbdce0d7946b5cd38997f6327c92
Derek Beckford <dbeck1207@icloud.com>
2021-08-16 19:52:18 -0500

    user-routes logic added

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+const { User, Comment } = require("../../models");
+
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: { exclude: ["password"] },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+    User.findOne({
+      attributes: { exclude: ["password"] },
+      where: {
+        id: req.params.id,
+      },
+      //add comments model
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
