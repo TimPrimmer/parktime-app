cb1e86239b352bd27b6a68e9c0429d86b21ff0f4
Derek Beckford <dbeck1207@icloud.com>
2021-08-16 19:32:32 -0500

    few code fixes

--- models/Users.js
+++ models/Users.js
@@ -2,15 +2,12 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-// create our User model
 class User extends Model {
-  // set up method to run on instance data (per user) to check password
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
-// create fields/columns for User model
 User.init(
   {
     id: {
@@ -41,7 +38,6 @@ User.init(
   },
   {
     hooks: {
-      // set up beforeCreate lifecycle "hook" functionality
       async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
