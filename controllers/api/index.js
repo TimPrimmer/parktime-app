2553ef7b459f99346652693d9a75ef3f09eb94a9
Erin Holmes <e.holmes09@yahoo.com>
2021-08-19 00:38:14 -0500

    remove category-routes file & reference in api/index - not a needed route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,12 +4,10 @@ const userRoutes = require("./user-routes.js");
 const parkRoutes = require("./park-routes.js");
 const commentRoutes = require("./comment-routes");
 const savedparkRoutes = require("./savedparks-routes");
-const categoriesRoutes = require("./category-routes");
 
 router.use("/users", userRoutes);
 router.use("/parks", parkRoutes);
 router.use("/comments", commentRoutes);
 router.use("/saved", savedparkRoutes); // shorted it to saved for convinience sake
-router.use("/categories", categoriesRoutes);
 
 module.exports = router;
