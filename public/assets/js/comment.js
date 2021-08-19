0cc4e6c3fb0147250c6cc92338aafadbf878899b
Derek Beckford <dbeck1207@icloud.com>
2021-08-18 22:59:58 -0500

    added ability to add comments

--- public/assets/js/comment.js
+++ public/assets/js/comment.js
@@ -1,12 +1,8 @@
-async function commentFormHandler(event) {
-  event.preventDefault();
+async function commentFormHandler(park_id) {
 
   const comment_text = document
-    .querySelector('textarea[name="comment-body"]')
+    .querySelector(`textarea[name="comment-body-${park_id}"]`)
     .value.trim();
-  const park_id = window.location.toString().split("/")[
-    window.location.toString().split("/").length - 1
-  ];
 
   if (comment_text) {
     const response = await fetch("/api/comments", {
@@ -20,14 +16,10 @@ async function commentFormHandler(event) {
       },
     });
 
-    if (response.ok) {
-      document.location.reload();
-    } else {
-      alert(response.statusText);
-    }
+  //   if (response.ok) {
+  //     document.location.reload();
+  //   } else {
+  //     alert(response.statusText);
+  //   }
   }
 }
-
-document
-  .querySelector(".comment-form")
-  .addEventListener("submit", commentFormHandler);
